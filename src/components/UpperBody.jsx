cf40eb46f2e59e30fa9f446e4e85beb995bf9e36
Saad Qureshi <muhammad.saadq14@gmail.com>
2022-12-04 21:01:49 -0500

    added 'onClick' function

--- src/components/UpperBody.jsx
+++ src/components/UpperBody.jsx
@@ -1,19 +1,34 @@
 import React from "react";
+import memesData from "../memesData";
 
 function UpperBody() {
+  function getImage() {
+    const memesArray = memesData.data.memes;
+    const randomNum = Math.floor(Math.random() * memesArray.length);
+    const url = memesArray[randomNum].url;
+    console.log(url);
+  }
+
   return (
     <div>
-      <form className="form">
+      <div className="form">
         <input type="text" className="form__input_top" placeholder="Top Text" />
         <input
           type="text"
           className="form__input_bottom"
           placeholder="Bottom Text"
         />
-        <button type="button" class="btn btn-primary" className="form__button">
-          <i class="bi bi-image">Generate a Meme</i>
+        <button
+          onClick={getImage}
+          type="button"
+          class="btn btn-primary"
+          className="form__button"
+        >
+          <i class="bi bi-image">Generate a meme image</i>
         </button>
-      </form>
+
+        <img src={getImage} />
+      </div>
     </div>
   );
 }
